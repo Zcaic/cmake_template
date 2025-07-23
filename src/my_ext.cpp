1cc2bd888a017ddde26e3d5ae3d9d584f5446bed
Zcaic <462845148@qq.com>
2025-07-23 16:43:07 +0800

    add overload

--- src/my_ext.cpp
+++ src/my_ext.cpp
@@ -9,8 +9,12 @@ int add(int a, int b=1) { return a + b; }
 void mul(nb::DRef<Eigen::MatrixXf> x,int b=2){
     x*=b;
 }
+void mul(nb::DRef<Eigen::MatrixXd> x,int b=2){
+    x*=b;
+}
 
 NB_MODULE(my_ext, m) {
     m.def("add", &add,nb::arg("a"),nb::arg("b")=1,"This function adds two numbers and increments if only one is provided.");
-    m.def("mul",&mul,nb::arg("x"),nb::arg("b")=2,"This function mul a numbers to a matrix without copy.");
+    m.def("mul",nb::overload_cast<nb::DRef<Eigen::MatrixXf>,int>(&mul),nb::arg("x"),nb::arg("b")=2,"This function mul a numbers to a matrix without copy.");
+    m.def("mul",nb::overload_cast<nb::DRef<Eigen::MatrixXd>,int>(&mul),nb::arg("x"),nb::arg("b")=2,"This function mul a numbers to a matrix without copy.");
 }
