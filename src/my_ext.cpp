1f6345dfde4218b75b3c95432cb44662c624bcc0
Zcaic <462845148@qq.com>
2025-07-23 12:32:59 +0800

    support eigen to python

--- src/my_ext.cpp
+++ src/my_ext.cpp
@@ -1,10 +1,16 @@
 #include <nanobind/nanobind.h>
+#include<nanobind/eigen/dense.h>
 
 namespace nb=nanobind;
 using namespace nb::literals;
 
 int add(int a, int b=1) { return a + b; }
 
+void mul(nb::DRef<Eigen::MatrixXf> x,int b=2){
+    x*=b;
+}
+
 NB_MODULE(my_ext, m) {
     m.def("add", &add,nb::arg("a"),nb::arg("b")=1,"This function adds two numbers and increments if only one is provided.");
+    m.def("mul",&mul,nb::arg("x"),nb::arg("b")=2,"This function mul a numbers to a matrix without copy.");
 }
